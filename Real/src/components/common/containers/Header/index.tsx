cf641afd26df9e9219fb53dfdc2559c25e025679
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-02-02 17:18:33 -0300

    Agregado imagen REAL con video de fondo

--- Real/src/components/common/containers/Header/index.tsx
+++ Real/src/components/common/containers/Header/index.tsx
@@ -1,8 +1,23 @@
+import '../../../../styles/header/header.css';
+
 const Header = () => {
-    return <div>
-      <img src="../../../public/video.png" alt="" />
-    </div>
+    return headerhtml;
   }
+
+  const headerhtml = (
+    <div className="header">
+        <div className="bg-black">
+            <div className="black-space"></div>
+            <img src="real-02.svg" className="real-image"/>
+            <div className="black-space"></div>
+        </div>
+          
+          <iframe className="yt-video-real" 
+        width="100%" height="100%" 
+        src="https://www.youtube.com/embed/cH7VBI4QQzA?autoplay=1&controls=0&mute=1&loop=1">
+        </iframe>
+    </div>
+  );
   
   export default Header
   
